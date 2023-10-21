3bf78847e1c097e491b087bd9a6bf2f6fc1416bd
Serhii Vostrikov <65418387+Antiserggio@users.noreply.github.com>
2023-10-21 22:15:14 +0300

    Summary

--- src/js/markup-categories.js
+++ src/js/markup-categories.js
@@ -1,35 +1,11 @@
 // Даний файл робить експорт функції createCategoriesMarkup()
-import axios from 'axios';
 
-const URL = 'https://your-energy.b.goit.study/api';
-const params = new URLSearchParams({
-  page: 1,
-  limit: 100,
-});
-
-axios.defaults.baseURL = 'https://your-energy.b.goit.study/api';
-
-const catGallery = document.querySelector('.js-categories-gallery');
-
-fethCards().then(data => {
-  createMarkupOptions(data.results);
-});
-
-function fethCards() {
-  return axios.get(`${URL}/filters?${params}`).then(response => {
-    if (response.status !== 200) {
-      throw new Error(response.status);
-    }
-
-    return response.data;
-  });
-}
+// const catGallery = document.querySelector('.js-categories-gallery');
 
 function createMarkupOptions(data) {
   const option = data
     .map(
       ({ filter, name, imgURL }) => `
-<ul class="categories-list">
   <li class="categories-item">
     <div class="categories-photo-container">
       <a class="categories-gradient" href="#">     
@@ -46,9 +22,10 @@ function createMarkupOptions(data) {
       <p class="categories-info-filter">${filter}</p>
      </div>
     </div>
-  </li>
-</ul>`
+  </li>`
     )
     .join('');
-  catGallery.insertAdjacentHTML('beforeend', option);
+  // catGallery.insertAdjacentHTML('beforeend', option);
 }
+
+export { createMarkupOptions };
