fcb406e24d09f4dffc0b9265b1c99b88c9e17b7e
AliceBondMe <ellie.bond.ff@gmail.com>
2023-10-22 15:33:07 +0300

    rating-finish

--- src/js/modal-rating.js
+++ src/js/modal-rating.js
@@ -1,5 +1,24 @@
 // Коли буде картка товару - замінити кнопку на відкриття рейтингу
+import axios from "axios";
 import { serviceRatingAdd } from "./api-service";
+import Notiflix from "notiflix";
+
+const notiflixParams = {
+    width: '320px',
+    position: 'center-center',
+    fontSize: '16px',
+    cssAnimationStyle: 'from-bottom',
+    backOverlay: true,
+    useIcon: false,
+    failure: {
+        background: '#242424',
+        textColor: '#F4F4F4',
+    },
+    success: {
+        background: '#F4F4F4',
+        textColor: '#242424',
+    },
+};
 
 const refs = {
     openRatingModalBtn: document.querySelector("[rating-modal-open]"),
@@ -9,31 +28,42 @@ const refs = {
     ratingStars: document.querySelectorAll(".rating-container>input"),
 };
 
-console.log(refs.ratingStars);
 
-
-refs.openRatingModalBtn.addEventListener("click", toggleModal); 
-refs.closeRatingModalBtn.addEventListener("click", toggleModal);
+refs.openRatingModalBtn.addEventListener("click", toggleRatingModal); 
+refs.closeRatingModalBtn.addEventListener("click", toggleRatingModal);
 refs.ratingForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(evt) {
     evt.preventDefault();
+    const workoutId = refs.openRatingModalBtn.closest(".ex-mod").dataset.id;
     const checkedStar = [...refs.ratingStars].find((star) => star.checked);
     const { email, message } = evt.currentTarget.elements;
     if (email.value.trim() === "" || message.value.trim() === "" || !checkedStar ) {
-        return alert("All fields must be filled!");
+        return Notiflix.Notify.failure("All fields must be filled!", notiflixParams);
     }
     const ratingObj = {
         rate: Number(checkedStar.value),
         email: email.value.trim(),
         review: message.value.trim(),
     };
-    console.dir(ratingObj);
 
-    evt.currentTarget.reset();
-    checkedStar.checked = false;
+    serviceRatingAdd(workoutId, ratingObj)
+        .then(() => 
+            Notiflix.Notify.success("Thank you for rating this exercise!", notiflixParams))
+        .catch((error) => {
+            if (error.response && error.response.status === 409) {
+                Notiflix.Notify.failure("You have already rated this workout", notiflixParams);
+            } else {
+                Notiflix.Notify.failure("Something went wrong. Please try again later.", notiflixParams);
+            }
+        })
+        .finally(() => {
+            refs.ratingForm.reset();
+            checkedStar.checked = false;
+            toggleRatingModal();
+        })
 }
 
-function toggleModal() {
+function toggleRatingModal() {
     refs.ratingModal.classList.toggle("is-hidden");
 };
